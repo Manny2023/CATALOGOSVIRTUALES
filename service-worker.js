8d325f514996ba0bcab8f6f9d758dfbe6664c858
Manny2023 <120243119+Manny2023@users.noreply.github.com>
2024-10-20 14:08:48 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,18 +14,35 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['my-cache'];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match('/index.html');
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Si hay una respuesta en la caché, la devuelve
+        if (response) {
+          return response;
+        }
+        // Si no hay respuesta en caché, intenta hacer la petición
+        return fetch(event.request).catch(() => {
+          // Si la petición falla, puedes manejar el error aquí
+          return caches.match('/index.html');
+        });
       })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  }
+  );
 });
