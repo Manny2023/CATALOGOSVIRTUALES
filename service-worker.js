8f78a60f1036af4d22aeafd8a6e8e607ac6c9849
Manny2023 <120243119+Manny2023@users.noreply.github.com>
2024-10-20 21:39:20 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,7 @@
-const CACHE_NAME = 'catalogos-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './styles.css',
-    './script.js',
-];
-
-// Evento install
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
-            .then(() => self.skipWaiting())
-    );
-});
-
-// Evento activate
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Evento fetch
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
-    );
-});
+<script>
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('worker.js?'+Math.random()+'')
+  });
+}
+</script>
