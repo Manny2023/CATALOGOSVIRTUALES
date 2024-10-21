44e60f70b947ac029ec249f55c13a7a73ae33cd4
Manny2023 <120243119+Manny2023@users.noreply.github.com>
2024-10-20 23:29:12 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,7 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
-  event.waitUntil(
-    caches.open('static-v1').then((cache) => {
-      return cache.addAll([
-        '/index.html',
-        '/manifest.json',
-        '/ruta-al-logo/favicon.ico',
-        '/ruta-al-service-worker/service-worker.js'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+<script>
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('worker.js?'+Math.random()+'')
+  });
+}
+</script>
